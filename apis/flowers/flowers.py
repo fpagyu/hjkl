5099e7e808857547f48d6ea9beb5c480ad1bf959
fpgayu <fpgayu@163.com>
2018-08-01 18:24:46 +0800

    # up

--- apis/flowers/flowers.py
+++ apis/flowers/flowers.py
@@ -4,8 +4,6 @@ from werkzeug.utils import secure_filename
 
 from apps import app
 
-from apps.flowers.services.flowers import FlowerService
-
 
 @app.route("/flowers/testing")
 def testing():
@@ -24,24 +22,50 @@ def testing():
     return "aaaa"
 
 
-@app.route("/flowers/<int:flower_id>")
-def get_flower(flower_id):
-    flower = FlowerService.get_by_pk(flower_id)
-    if flower is None:
-        return jsonify({
-            'ok': False,
-            'code': 4004,
-            'msg': 'not found'
-        })
+@app.route("/flowers/login")
+def login():
+    # 获取微信openid进行登录, 进入小程序即登录
+    pass
 
-    data = {
-        'id': flower.id,
-        'name': flower.name,
-        'path': flower.path,
-        'content': flower.content
-    }
 
-    return jsonify({
-        'ok': True,
-        'data': data
-    })
+@app.route("/flowers/orders")
+def get_orders():
+    # 获取用户订单列表
+    pass
+
+
+@app.route("/flowers/orders/<order_id>")
+def get_order_detail():
+    # 获取订单详情
+    pass
+
+
+@app.route("/flowers/orders")
+def post_order():
+    # 提交订单
+    pass
+
+
+@app.route("/flowers/orders")
+def put_order():
+    # 修改订单状态
+    pass
+
+
+@app.route("/flowers/banner")
+def flowers_banner():
+    # 获取banner数据
+    pass
+
+
+@app.route("/flowers/filter")
+def goods_filter():
+    # 根据分类获取商品列表
+    pass
+
+
+@app.route("/flowers/goods")
+def post_goods():
+    # 编辑商品
+    pass
+
